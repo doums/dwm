c1f68407bb71b28242effb6176cdb495eb37d175
pierre <dommerc.pierre@gmail.com>
2020-06-20 01:14:45 +0200

    config

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 14;        /* gaps between windows */
-static const int bar_pa             = 4;        /* pixel vertical padding of bar */
+static const int bar_pa             = 6;        /* pixel vertical padding of bar */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "$", "www", "IDE", "slack", "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
